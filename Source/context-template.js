484549d3801b5f6d4718631a79a2f5cb8abf6e8d
Maksim Horbachevsky <fantactuka@gmail.com>
2014-02-22 23:12:01 +0300

    Indents & amd for 3 y.o. code

--- Source/context-template.js
+++ Source/context-template.js
@@ -1,56 +1,69 @@
-(function() {
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    window.ContextTemplate = factory();
+  }
+})(function() {
 
-    var CT = window.ContextTemplate = function(html, variables) {
-		var hashCode = toHashCode(html);
-		
-        if (!CT.cache[hashCode]) {
-            CT.cache[hashCode] = new Function(CT.compile(html));
-        }
+  var CT = function(html, variables) {
+    var hashCode = toHashCode(html);
 
-        return CT.cache[hashCode].apply(variables || {});
-    };
+    if (!CT.cache[hashCode]) {
+      CT.cache[hashCode] = new Function(CT.compile(html));
+    }
 
-	var toHashCode = function(str) {
-	    var h1 = (5381 << 16) + 5381, h2 = h1, index = 0;
-	    while (index < str.length) {
-	        h1 = ((h1 << 5) + h1 + (h1 >> 27)) ^ str.charCodeAt(index);
-	        if (index == str.length - 1) {
-	            break;
-	        }
-	        h2 = ((h2 << 5) + h2 + (h2 >> 27)) ^ str.charCodeAt(index + 1);
-	        index += 2;
-	    }
- 
-	    return h1 + (h2 * 1566083941);
-	}
+    return CT.cache[hashCode].apply(variables || {});
+  };
 
-    var evaluation = function(match, code) {
-        return "');" + code.replace(/\\'/g, "'").replace(/[\r\n\t]/g, ' ') + "p.push('";
-    };
+  var toHashCode = function(str) {
+    var h1 = (5381 << 16) + 5381,
+        h2 = h1,
+        index = 0;
 
-    var interpolation = function(match, code) {
-        return "'," + code.replace(/\\'/g, "'").replace(/([\W])@(\w)/, "$1this.$2") + ",'";
-    };
+    while (index < str.length) {
+      h1 = ((h1 << 5) + h1 + (h1 >> 27)) ^ str.charCodeAt(index);
+      if (index == str.length - 1) {
+        break;
+      }
+      h2 = ((h2 << 5) + h2 + (h2 >> 27)) ^ str.charCodeAt(index + 1);
+      index += 2;
+    }
 
-    CT.cache = {};
+    return h1 + (h2 * 1566083941);
+  };
 
-    CT.compile = function(html) {
-        var settings = CT.settings,
-                result = html
-                        .replace(/\\/g, '\\\\')
-                        .replace(/'/g, "\\'")
-                        .replace(settings.interpolate, interpolation)
-                        .replace(settings.evaluate || null, evaluation)
-                        .replace(/\r/g, '\\r')
-                        .replace(/\n/g, '\\n')
-                        .replace(/\t/g, '\\t');
+  var evaluation = function(match, code) {
+    return "');" + code.replace(/\\'/g, "'").replace(/[\r\n\t]/g, ' ') + "p.push('";
+  };
 
-        return "var p = []; p.push('" + result + "'); return p.join('')";
-    };
+  var interpolation = function(match, code) {
+    return "'," + code.replace(/\\'/g, "'").replace(/([\W])@(\w)/, "$1this.$2") + ",'";
+  };
 
-    CT.settings = {
-        evaluate: /<%([\s\S]+?)%>/g,
-        interpolate: /<%=([\s\S]+?)%>/g
-    };
+  CT.cache = {};
 
-})();
+  CT.compile = function(html) {
+    var settings = CT.settings,
+        result = html
+            .replace(/\\/g, '\\\\')
+            .replace(/'/g, "\\'")
+            .replace(settings.interpolate, interpolation)
+            .replace(settings.evaluate || null, evaluation)
+            .replace(/\r/g, '\\r')
+            .replace(/\n/g, '\\n')
+            .replace(/\t/g, '\\t');
+
+    return "var p = []; p.push('" + result + "'); return p.join('')";
+  };
+
+  CT.settings = {
+    evaluate: /<%([\s\S]+?)%>/g,
+    interpolate: /<%=([\s\S]+?)%>/g
+  };
+
+  return CT;
+
+});
